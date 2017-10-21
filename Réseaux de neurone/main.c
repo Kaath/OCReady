e1e8ffb3bbc704c3024d0d651dbadff7d4bcb5fb
Arthur Desmoulins <arthur.desmoulins@epita.fr>
2017-10-21 14:35:01 -0400

    commit du futur

--- Réseaux de neurone/main.c
+++ Réseaux de neurone/main.c
@@ -2,51 +2,65 @@
 # include <stdlib.h>
 
 # include "operations.h"
-# include "main.h"
 
 
-float *forward(float weightsInput[], float weightsOutput[], float inputs[], size_t HiddenUnits){
-  size_t = 4*HiddenUnits;
-  float hiddenlayersum[size] = mul(inputs, weightsInput, 4, 2, HiddenUnits);
-  float hiddenlayerresult[size] = apply(sigmoid, hiddenlayersum, size);
-  float outputsum[4] = mul(hiddenlayerresult, weightsOutput, 4, HiddenUnits, 1);
-  float outputresult[4] = apply(sigmoid, outputsum, 4);
+float **forward(float weightsInput[], float weightsOutput[], float inputs[], size_t HiddenUnits) {
+  size_t size = 4*HiddenUnits;
+  float *hiddenlayersum = mul(inputs, weightsInput, 4, 2, HiddenUnits);
+  float *hiddenlayerresult = apply(sigmoid, hiddenlayersum, size);
+  float *outputsum = mul(hiddenlayerresult, weightsOutput, 4, HiddenUnits, 1);
+  float *outputresult = apply(sigmoid, outputsum, 4);
 
-  float *forwardresults[4] = {hiddenlayersum, hiddenlayerresult, outputsum, outputresult};
+  float **forwardresults = malloc(4 * sizeof(float*));
+  forwardresults[0] = hiddenlayersum;
+  forwardresults[1] = hiddenlayerresult;
+  forwardresults[2] = outputsum;
+  forwardresults[3] = outputresult;
   return forwardresults;
 }
 
 
-float *backward(float weightsInput[], float weightsOutput[], float LearningRate, float forwardresults[], size_t HiddenUnits, float inputs[]){
+float **backward(float weightsInput[], float weightsOutput[], float LearningRate, float *forwardresults[], size_t HiddenUnits, float inputs[]){
   float expectedoutputs[4] = {0,1,1,0};
-  float outputerror[4] = substract(expectedoutputs, *forwardresults[3], 4, 1);
-  float deltaoutputerror = dot(apply(prime, *forwardresults[2], 4), outputerror, 4, 1);
-  float hiddenoutputchanges = scalar(multiply(transpose(forwardresults[1], 4, HiddenUnits), deltaoutputerror, HiddenUnits, 4, 1), LearningRate);
-  float deltahiddenerror = dot(multiply(deltaoutputerror, transpose(weightsOutput, HiddenUnits, 1), 4, 1, HiddenUnits), sigmoidprime(*forwardresults[0], 4*HiddenUnits), 4, HiddenUnits);
-  float inputhiddenchanges = scalar(multiply(transpose(inputs, 8), deltahiddenerror, 2, 4, HiddenUnits), LearningRate);
+  float *outputerror = substract(expectedoutputs, forwardresults[3], 4, 1);
+
+  float *apforout = apply(sigmoidprime, forwardresults[2], 4);
+  float *deltaoutputerror = dot(apforout, outputerror, 4, 1);
+
+  float *transfor = transpose(forwardresults[1], 4, HiddenUnits);
+  float *multransdelta = mul(transfor, deltaoutputerror, HiddenUnits, 4, 1);
+  float *hiddenoutputchanges = scalar(multransdelta, LearningRate, 4, 1);
+
+  float *transweiOut = transpose(weightsOutput, HiddenUnits, 1);
+  float *muldeltranweiOut = mul(deltaoutputerror, transweiOut, 4, 1, HiddenUnits);
+  float *deltahiddenerror = dot(muldeltranweiOut, apply(sigmoidprime, forwardresults[0], 4*HiddenUnits), 4, HiddenUnits);
+  float *inputhiddenchanges = scalar(mul(transpose(inputs, 8, 2), deltahiddenerror, 2, 4, HiddenUnits), LearningRate, 2, 1);
 
   weightsInput = add(weightsInput, inputhiddenchanges, 2, HiddenUnits); //est-ce que ça marche? pas sur
   weightsOutput = add(weightsOutput, hiddenoutputchanges, HiddenUnits, 1);
 
-  float *backwardresults[2] = {deltahiddenerror, deltaoutputerror};
-  }
+  float **backwardresults = malloc(2 * sizeof(float*));
+  backwardresults[0] = deltahiddenerror;
+  backwardresults[1] = deltaoutputerror;
+  return backwardresults;
 }
 
 
 int main(int argc, char *argv[])
 {
-  float LearningRate = argv[0];
-  size_t Iterations = argv[1];
-  size_t HiddenUnits = argv[2];
+  float LearningRate = atof(argv[0]);
+  size_t Iterations = (size_t)argv[1];
+  size_t HiddenUnits = (size_t)argv[2];
   float inputs[8] = {0,0,0,1,1,0,1,1};
   float weightsInput[2*HiddenUnits]; // mettre des valeurs flottantes random entre 0 et 1
   float weightsOutput[HiddenUnits]; // mettre des valeurs flottantes random entre 0 et 1
 
   for (size_t i = 0; i < Iterations; i++){
-    float* forwardresults = forward(weightsInput, weightsOutput, inputs, HiddenUnits); //Vérifier que les weights sont bien en reference et seront modifiés
-    float* backwardresults = backward(weightsInput, weightsOutput, LearningRate, forwardresults, HiddenUnits , inputs);
+    float** forwardresults = forward(weightsInput, weightsOutput, inputs, HiddenUnits); //Vérifier que les weights sont bien en reference et seront modifiés
+    float** backwardresults = backward(weightsInput, weightsOutput, LearningRate, forwardresults, HiddenUnits , inputs);
     // ajouter ce qu'on veut print pendant la soutenance, genre le résultat pour chaque input à chaque itération et l'écart du résultat optenu.
   }
+
 
   return 0;
 }
--- Réseaux de neurone/operations.h
+++ Réseaux de neurone/operations.h
@@ -1,23 +1,25 @@
 #ifndef _OPERATIONS_H_
 #define _OPERATIONS_H_
 
+typedef float func_t(float);
+
 // operations functions
 
 float sigmoid(float x);
 
 float sigmoidprime(float x);
 
-float *mul(float inputs, float weights, size_t n, size_t m, size_t p);
+float *mul(float inputs[], float weights[], size_t n, size_t m, size_t p);
 
 float *apply(func_t f, float values[], size_t size);
 
-float *substract(float output[] , float outputresult[], size_t lines, size_t cols);
+float *substract(float output[], float outputresult[], size_t lines, size_t cols);
 
 float *add(float output[] , float outputresult[], size_t lines, size_t cols);
 
 float *transpose(float mat[], size_t lines, size_t cols);
 
-float *scalar(float mat[], float LearningRate);
+float *scalar(float mat[], float LearningRate, size_t lines, size_t cols);
 
 float *dot(float prime[] , float error[], size_t lines, size_t cols);
 
