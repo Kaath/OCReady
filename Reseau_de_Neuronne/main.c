2e273508bb5d4bc1d0ebf346a0696e92ff317b26
Clément Gonnaud <clement.gonnaud@epita.fr>
2017-12-08 14:12:13 +0100

    nN

--- Reseau_de_Neuronne/main.c
+++ Reseau_de_Neuronne/main.c
@@ -5,25 +5,22 @@
 
 # include "operations.h"
 
-void print_matrix(float mat[], size_t lines, size_t cols)
-{
-  for (size_t i = 0; i<lines; i++)
-  {
-    for (size_t j = 0; j<cols; j++)
-    {
-      printf("%.4f   ", mat[i*cols +j]);
+char treatment(float output[]){
+  size_t x = 0;
+  for (size_t i = 0; i<26; i++){
+    if (output[i] > output[x]){
+      x = i;
     }
-    printf("\n");
   }
-  printf("\n");
+  return x + 97;
 }
 
 float **forward(float weightsInput[], float weightsOutput[], float inputs[], size_t HiddenUnits) {
   size_t size = HiddenUnits;
-  float *hiddenlayersum = mul(inputs, weightsInput, 1, 10, HiddenUnits);
+  float *hiddenlayersum = mul(inputs, weightsInput, 1, 784, HiddenUnits);
   float *hiddenlayerresult = apply(sigmoid, hiddenlayersum, size);
-  float *outputsum = mul(hiddenlayerresult, weightsOutput, 1, HiddenUnits, 10);
-  float *outputresult = apply(sigmoid, outputsum, 10);
+  float *outputsum = mul(hiddenlayerresult, weightsOutput, 1, HiddenUnits, 26);
+  float *outputresult = apply(sigmoid, outputsum, 26);
 
   float **forwardresults = malloc(4 * sizeof(float*));
   forwardresults[0] = hiddenlayersum;
@@ -34,26 +31,24 @@ float **forward(float weightsInput[], float weightsOutput[], float inputs[], siz
 }
 
 
-float **backward(float weightsInput[], float weightsOutput[], float LearningRate, float *forwardresults[], size_t HiddenUnits, float inputs[]){
-  float expectedoutputs[] = {1,0,1,0,1};
-  float *outputerror = substract(expectedoutputs, forwardresults[3], 1, 5);
+float **backward(float weightsInput[], float weightsOutput[], float LearningRate, float *forwardresults[], size_t HiddenUnits, float inputs[], float output[]){
+  float *outputerror = substract(output, forwardresults[3], 1, 26);
 
-  float *apforout = apply(sigmoidprime, forwardresults[2], 5);
-  float *deltaoutputerror = dot(apforout, outputerror, 1, 5);
+  float *apforout = apply(sigmoidprime, forwardresults[2], 26);
+  float *deltaoutputerror = dot(apforout, outputerror, 1, 26);
 
   float *transfor = transpose(forwardresults[1], 1, HiddenUnits);
-  float *multransdelta = mul(transfor, deltaoutputerror, HiddenUnits, 1, 5);
+  float *multransdelta = mul(transfor, deltaoutputerror, HiddenUnits, 1, 26);
   float *hiddenoutputchanges = scalar(multransdelta, LearningRate, HiddenUnits, 1);
 
-  float *transweiOut = transpose(weightsOutput, HiddenUnits, 5);
-  float *muldeltranweiOut = mul(deltaoutputerror, transweiOut, 1, 5, HiddenUnits);
+  float *transweiOut = transpose(weightsOutput, HiddenUnits, 26);
+  float *muldeltranweiOut = mul(deltaoutputerror, transweiOut, 1, 26, HiddenUnits);
   float *deltahiddenerror = dot(muldeltranweiOut, apply(sigmoidprime, forwardresults[0], HiddenUnits), 1, HiddenUnits);
-  float *multransinde = mul(transpose(inputs, 1, 10), deltahiddenerror, 10, 1, HiddenUnits);
-  float *inputhiddenchanges = scalar(multransinde, LearningRate, 10, HiddenUnits);
+  float *multransinde = mul(transpose(inputs, 1, 784), deltahiddenerror, 784, 1, HiddenUnits);
+  float *inputhiddenchanges = scalar(multransinde, LearningRate, 784, HiddenUnits);
 
-  weightsInput = add(weightsInput, inputhiddenchanges, 10, HiddenUnits);
-  weightsOutput = add(weightsOutput, hiddenoutputchanges, HiddenUnits, 5);
-
+  weightsInput = add(weightsInput, inputhiddenchanges, 784, HiddenUnits);
+  weightsOutput = add(weightsOutput, hiddenoutputchanges, HiddenUnits, 26);
 
   float **backwardresults = malloc(2 * sizeof(float*));
   backwardresults[0] = weightsInput;
@@ -63,61 +58,61 @@ float **backward(float weightsInput[], float weightsOutput[], float LearningRate
 }
 
 
-int main(int argc, char *argv[])
+char learn(float inputs[], char expectedoutput)
 {
-  if (argc != 4){
-    errx(1, "Wrong number of arguments, %d given, 4 expected", argc);
+  float output[26];
+  for (int i = 0; i<26; i++){
+    if (i + 97 == expectedoutput){
+      output[i] = 1;
+    }
+    else {
+      output[i] = 0;
+    }
   }
-  else{
-    float LearningRate = atof(argv[1]);
-    size_t Iterations = (size_t)atoi(argv[2]);
-    size_t HiddenUnits = (size_t)atoi(argv[3]);
-    float inputs[] = {1,0,0,0,0,0,0,0};
-    float weightsInput[10*HiddenUnits];
-    float weightsOutput[5*HiddenUnits];
 
-    FILE *r = fopen("weightsIn", "rb");
-    if (r != NULL) {
-        fread(weightsInput, sizeof(float), 10*HiddenUnits, r);
+  float LearningRate = 0.7;
+  size_t HiddenUnits = 50;
+  float weightsInput[784*HiddenUnits];
+  float weightsOutput[26*HiddenUnits];
+
+  FILE *r = fopen("weightsIn", "rb");
+  if (r != NULL) {
+      if (fread(weightsInput, sizeof(float), 784*HiddenUnits, r)){
         fclose(r);
         printf("file 1 read\n");
-    }
+      }
+  }
 
-    FILE *r2 = fopen("weightsOut", "rb");
-    if (r2 != NULL) {
-        fread(weightsOutput, sizeof(float), 5*HiddenUnits, r2);
+  FILE *r2 = fopen("weightsOut", "rb");
+  if (r2 != NULL) {
+      if (fread(weightsOutput, sizeof(float), 26*HiddenUnits, r2)){
         fclose(r2);
         printf("file 2 read\n");
-    }
-    else{
-        srand((unsigned int)time(NULL));
-
-        for (size_t i = 0; i < 10*HiddenUnits; i++) {
-          weightsOutput[i] = (float)rand()/(float)RAND_MAX;
-          weightsInput[i] = (float)rand()/(float)RAND_MAX;
-        }
-    }
-
-    for (size_t i = 0; i < Iterations; i++){
-      float** forwardresults = forward(weightsInput, weightsOutput, inputs, HiddenUnits);
-      float** backwardresults = backward(weightsInput, weightsOutput, LearningRate, forwardresults, HiddenUnits , inputs);
-      for (size_t i = 0; i < 2*HiddenUnits; i++) {
-        if (i < HiddenUnits) {
-            weightsOutput[i] = backwardresults[1][i];
-          }
-          weightsInput[i] = backwardresults[0][i];
-        }
-      if ((i%1000 == 999) || (i == 0)) {
-        printf("For %zu iterations :\n", i+1);
-        printf("Inputs 0,0 -> Output = %f\n", forwardresults[3][0]);
-        printf("Inputs 0,1 -> Output = %f\n", forwardresults[3][1]);
-        printf("Inputs 1,0 -> Output = %f\n", forwardresults[3][2]);
-        printf("Inputs 1,1 -> Output = %f\n", forwardresults[3][3]);
-        printf("Inputs 1,1 -> Output = %f\n\n", forwardresults[3][4]);
-
       }
-    }
+  }
+  else{
+      srand((unsigned int)time(NULL));
 
+      for (size_t i = 0; i < 26*HiddenUnits; i++) {
+        weightsOutput[i] = (float)rand()/(float)RAND_MAX;
+      }
+      for (size_t i = 0; i < 784*HiddenUnits; i++) {
+        weightsInput[i] = (float)rand()/(float)RAND_MAX;
+      }
+  }
+
+  float** forwardresults = forward(weightsInput, weightsOutput, inputs, HiddenUnits);
+  char result = treatment(forwardresults[3]);
+
+  float** backwardresults = backward(weightsInput, weightsOutput, LearningRate, forwardresults, HiddenUnits , inputs, output);
+  for (size_t i = 0; i < 784*HiddenUnits; i++) {
+    if (i < 26*HiddenUnits) {
+      weightsOutput[i] = backwardresults[1][i];
+    }
+    weightsInput[i] = backwardresults[0][i];
+  }
+  free(forwardresults);
+  free(backwardresults);
   FILE *f = fopen("weightsIn", "wb");
   fwrite(weightsInput, sizeof(float), 10*HiddenUnits, f);
   fclose(f);
@@ -125,6 +120,32 @@ int main(int argc, char *argv[])
   FILE *f2 = fopen("weightsOut", "wb");
   fwrite(weightsOutput, sizeof(float), 5*HiddenUnits, f2);
   fclose(f2);
-  return 0;
+
+  return result;
+}
+
+char result(float inputs[]){
+  size_t HiddenUnits = 50;
+  float weightsInput[784*HiddenUnits];
+  float weightsOutput[26*HiddenUnits];
+
+  FILE *r = fopen("weightsIn", "rb");
+  if (r != NULL) {
+      if (fread(weightsInput, sizeof(float), 784*HiddenUnits, r)){
+        fclose(r);
+        printf("file 1 read\n");
+      }
   }
+
+  FILE *r2 = fopen("weightsOut", "rb");
+  if (r2 != NULL) {
+      if (fread(weightsOutput, sizeof(float), 26*HiddenUnits, r2)){
+        fclose(r2);
+        printf("file 2 read\n");
+      }
+  }
+
+  float** forwardresults = forward(weightsInput, weightsOutput, inputs, HiddenUnits);
+  return treatment(forwardresults[3]);
+  free(forwardresults);
 }
